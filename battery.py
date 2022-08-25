f9d440ddd38471562fa7af3679d4e182ea94ab03
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-08-24 21:59:45 -0400

    refactor battery

---
+++ battery.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+class Battery(ABC):
+
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
+        self.current_date = datetime.today().date()
+
+    @abstractmethod
+    def needs_service(self):
+        pass
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date):
+        super().__init__(last_service_date)
+
+    def needs_service(self):
+        service_year = self.last_service_date.year + 2
+        service_threshold_date = self.last_service_date.replace(service_year)
+        return service_threshold_date < self.current_date
+
+class NubbinBattery(Battery):
+    def __init__(self, last_service_date):
+        super().__init__(last_service_date)
+
+    def needs_service(self):
+        service_year = self.last_service_date.year + 4
+        service_threshold_date = self.last_service_date.replace(service_year)
+        return service_threshold_date < self.current_d
+
